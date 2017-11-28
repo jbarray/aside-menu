8bec0d4f7ec9b5a2ecd9730feb62734c2c76815c
jby <1614963384@qq.com>
2017-11-28 14:50:44 +0800

    功能:鼠标放置在图标上时,图标更换成文字内容.

--- aside_menu.js
+++ aside_menu.js
@@ -2,27 +2,69 @@
 var arrIcon=document.getElementsByClassName('row');
 // var goToWeChat=document.getElementById('weChat-img');
 // var goToPhone=document.getElementById('to-phone-img');
-// var top=document.getElementById('firstContent');
+ var font=document.getElementsByClassName('hidden');
 
-//鼠标放置在图标上,显示照片
-function changePic(){
-        //显示微信图片
-        arrIcon[0].onmouseover=function(){
-            document.getElementById('weChat-img').style.display='block';
-        };
-        //离开消失
-        arrIcon[0].onmouseout=function(){
-            document.getElementById('weChat-img').style.display='none';
-        };
-        //显示APP下载图片
-        arrIcon[2].onmouseover=function (){
-            document.getElementById('to-phone-img').style.display ='block';
-        };
-        //离开消失
-        arrIcon[2].onmouseout=function(){
-            document.getElementById('to-phone-img').style.display='none';
-        };
+//goToTop相关操作
+//到达某个位置,goToTop图标消失
+function toTop(){
+    if(check){
+        document.getElementById('goToTop').style.display='none';
+
+    }
+    else{
+        arrIcon[3].onclick=function(){
+            document.getElementById('goToTop').style.display='none';
+        }
+    }
 }
-changePic();
-
-//
+function check() {
+    setInterval(function () {
+        var height = document.body.scrollTop;
+        var orderHeight = document.getElementById('order').offsetTop;
+        return (height < orderHeight);
+    }, 20)
+}
+toTop();
+//鼠标放置在图标上,图标消失,出现文字
+function change(){
+    font[0].onmouseout=function(){
+        document.getElementById('weChat-img').style.display='none';
+        arrIcon[0].style.display='block';
+        font[0].style.display='none';
+    };
+    font[1].onmouseout=function(){
+        arrIcon[1].style.display='block';
+        font[1].style.display='none';
+    };
+    font[2].onmouseout=function(){
+        document.getElementById('to-phone-img').style.display='none';
+        arrIcon[2].style.display='block';
+        font[2].style.display='none';
+    };
+    font[3].onmouseout=function(){
+        arrIcon[3].style.display='block';
+        font[3].style.display='none';
+    };
+}
+change();
+changeBack();
+function changeBack(){
+    arrIcon[0].onmouseover=function(){
+        document.getElementById('weChat-img').style.display='block';
+        this.style.display='none';
+        font[0].style.display='block';
+    };
+    arrIcon[1].onmouseover=function(){
+        this.style.display='none';
+        font[1].style.display='block';
+    };
+    arrIcon[2].onmouseover=function(){
+        document.getElementById('to-phone-img').style.display='block';
+        this.style.display='none';
+        font[2].style.display='block';
+    };
+    arrIcon[3].onmouseover=function(){
+        this.style.display='none';
+        font[3].style.display='block';
+    };
+}
