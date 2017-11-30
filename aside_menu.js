daa64767ba6c2584a33ffbb8b278cba9617356b7
jby <1614963384@qq.com>
2017-11-30 15:53:54 +0800

    修复问题:鼠标放置在图标上时,出现闪动.

--- aside_menu.js
+++ aside_menu.js
@@ -24,21 +24,21 @@ window.onscroll=function(){
 // }
 //鼠标放置在图标上,图标消失,出现文字
 function change(){
-    font[0].onmouseout=function(){
+    font[0].onmouseleave=function(){
         document.getElementById('weChat-img').style.display='none';
         arrIcon[0].style.display='block';
         font[0].style.display='none';
     };
-    font[1].onmouseout=function(){
+    font[1].onmouseleave=function(){
         arrIcon[1].style.display='block';
         font[1].style.display='none';
     };
-    font[2].onmouseout=function(){
+    font[2].onmouseleave=function(){
         document.getElementById('to-phone-img').style.display='none';
         arrIcon[2].style.display='block';
         font[2].style.display='none';
     };
-    font[3].onmouseout=function(){
+    font[3].onmouseleave=function(){
         arrIcon[3].style.display='block';
         font[3].style.display='none';
     };
@@ -46,21 +46,21 @@ function change(){
 change();
 changeBack();
 function changeBack(){
-    arrIcon[0].onmouseover=function(){
+    arrIcon[0].onmouseenter=function(){
         document.getElementById('weChat-img').style.display='block';
         this.style.display='none';
         font[0].style.display='block';
     };
-    arrIcon[1].onmouseover=function(){
+    arrIcon[1].onmouseenter=function(){
         this.style.display='none';
         font[1].style.display='block';
     };
-    arrIcon[2].onmouseover=function(){
+    arrIcon[2].onmouseenter=function(){
         document.getElementById('to-phone-img').style.display='block';
         this.style.display='none';
         font[2].style.display='block';
     };
-    arrIcon[3].onmouseover=function(){
+    arrIcon[3].onmouseenter=function(){
         this.style.display='none';
         font[3].style.display='block';
     };
